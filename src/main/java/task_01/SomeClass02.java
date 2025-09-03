9a794fa2d2c7c12a311e395a6f3d2090302bce70
konstantin-gaubatz <konstantin.gaubatz@gmail.com>
2025-09-04 01:31:48 +0200

    3 classes added

---
+++ src/main/java/task_01/SomeClass02.java
@@ -0,0 +1,9 @@
+package task_01;
+
+public class SomeClass02 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Код класса 2");
+    }
+}
---
+++ src/main/java/task_01/SomeClass03.java
@@ -0,0 +1,9 @@
+package task_01;
+
+public class SomeClass03 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Код класса 3");
+    }
+}
